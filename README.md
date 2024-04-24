126d3e148e1b09030cfe803ae72f7173673e228c
dennizzzz <dverbeek@hotmail.com>
2024-04-24 13:16:15 +0200

    Added metadata of audio media.

--- README.md
+++ README.md
@@ -1,19 +1,47 @@
 # castnow
 
 Castnow is a command-line utility that can be used to play back media files on
-your Chromecast device. It supports playback of local video files, videos on the web and torrents.
-You can also re-attach a running playback session \(this sentence should belong somewhere else).
+your Chromecast-enabled device. It supports playback of any
+[Chromecast supported media files
+](https://developers.google.com/cast/docs/media),
+videos on the web and torrents. If no media is passed as a parameter, castnow
+will re-attach to a running playback session.
+
+This version of castnow almost fixes the glaring omission of the Chromecast not
+using the metadata and cover art present in music files to display. The almost
+part being to only use a tiny version of the cover art to fit in the arbitrary
+maximum message size of 64k.
+
+Tested:
+
+- 2019.09.01 Chromecast V2, 1.40.156414
+- 2019.09.01 Sony STR-DN1080 / Chromecast Built-in, 1.21.76349
+
+Bonus:
+
+- Display cover art on the console if possible.
+- For KDE users a desktop icon file is included. It starts castnow when you
+drop an URL on it. There is a 254 in 255 chance you need to edit this for
+it to work though.
 
 ### Interested in being a castnow maintainer?
 
-I currently don't have that much time to maintain this project and have also lost some interest (to be honest).
-Main reason is that we have had a new TV for a few months that supports casting directly to it using
-DLNA \(you may wanna checkout [dlnacast](https://github.com/xat/dlnacast)).
-Feel free to contact me \( [simon@sope.io](simon@sope.io) ) if you want to be added as a maintainer to castnow.
+I currently don't have that much time to maintain this project and have also
+lost some interest (to be honest). Main reason is that we have had a new TV for
+a few months that supports casting directly to it using DLNA \(you may wanna
+checkout [dlnacast](https://github.com/xat/dlnacast)). Feel free to contact me
+\( [simon@sope.io](simon@sope.io) ) if you want to be added as a maintainer to
+castnow.
 
 ### Install
 
 `sudo npm install castnow -g`
+
+or ... for this extended version:
+
+- Clone/download from [github](https://github.com/dennizzzz/castnow)
+- Go to the cloned/unzipped folder
+- Run npm install to get all dependencies
 
 ### Usage
 
@@ -54,55 +82,211 @@ castnow
 ```
 
 ### Options
+<table>
+<tr>
+<td>
+<code>--tomp4 </code>
+</td>
+<td>
+Transcode a video file to mp4 during playback. This option requires ffmpeg to
+be installed on your computer. The play / pause controls are currently not
+supported in transcode mode.
+</td>
+</tr>
+<tr>
+<td>
+<code>--device "my chromecast" </code>
+</td>
+<td>
+If you have more than one Chromecast on your network, use this option to
+specify the device on which you want to start casting. Otherwise, castnow will
+just use the first device it finds in the network.
+</td>
+</tr>
+<tr>
+<td>
+<code>--address 192.168.1.4 </code>
+</td>
+<td>
+The IP address or hostname of your chromecast. This
+  will skip the MDNS scan and improve the initial response time.
+</td>
+</tr>
+<tr>
+<td>
+<code>--subtitles <path/URL> </code>
+</td>
+<td>
+This can be a path or URL to a vtt or srt file that contains subtitles.
+</td>
+</tr>
+<tr>
+<td>
+<code>--subtitle-scale 1.5 </code>
+</td>
+<td>
+Scaling factor for the size of the subtitle font. Default is 1.0.
+</td>
+</tr>
+<tr>
+<td>
+<code>--subtitle-color #FFFFFFFF </code>
+</td>
+<td>
+Foreground RGBA color of the subtitle font.
+</td>
+</tr>
+<tr>
+<td>
+<code>--myip 192.168.1.8 </code>
+</td>
+<td>
+Your main IP address. (Useful if you have multiple network adapters.)
+</td>
+</tr>
+<tr>
+<td>
+<code>--quiet </code>
+</td>
+<td>
+Hide the player timeline.
+</td>
+</tr>
+<tr>
+<td>
+<code>--peerflix-&lt;option&gt; &lt;argument&gt; </code>
+</td>
+<td>
+Pass options to peerflix.
+</td>
+</tr>
+<tr>
+<td>
+<code>--ffmpeg-&lt;option&gt; &lt;argument&gt; </code>
+</td>
+<td>
+ Pass options to ffmpeg.
+</td>
+</tr>
+<tr>
+<td>
+<code>--type <type> </code>
+</td>
+<td>
+Explicity set the mime-type of the first item in the playlist
+(e.g. 'video/mp4').
+</td>
+</tr>
+<tr>
+<td>
+<code>--seek <hh:mm:ss> </code>
+</td>
+<td>
+Seek to the specified time on start using the format hh:mm:ss or mm:ss.
+</td>
+</tr>
+<tr>
+<td>
+<code>--bypass-srt-encoding </code>
+</td>
+<td>
+Disable automatic UTF-8 encoding of SRT subtitles.
+</td>
+</tr>
+<tr>
+<td>
+<code>--loop </code>
+</td>
+<td>
+Play the list of files over and over in a loop, forever.
+</td>
+</tr>
+<tr>
+<td>
+<code>--shuffle </code>
+</td>
+<td>
+Play the list of files in random order.
+</td>
+</tr>
+<tr>
+<td>
+<code>--recursive </code>
+</td>
+<td>
+ List all files in directories recursively.
+</td>
+</tr>
+<tr>
+<td>
+<code>--volume-step </code>
+</td>
+<td>
+Step at which the volume changes. Helpful for speakers that are softer or
+louder than normal. Value ranges from 0 to 1. Default is 0.05.
+</td>
+</tr>
+<tr>
+<td>
+<code>--metadata false</code>
+</td>
+<td>
+Do not attempt to retrieve metadata from audio media.
+</td>
+</tr>
+<tr>
+<td>
+<code>--showmetadata</code>
+</td>
+<td>
+Show metadata.
+</td>
+</tr>
+<tr>
+<td>
+<code>--showcover false</code>
+</td>
+<td>
+Do not show cover art on console.
+</td>
+</tr>
+<tr>
+<td>
+<code>--showoptions</code>
+</td>
+<td>
+Show options.
+</td>
+</tr>
+<tr>
+<td>
+<code>--command &lt;key1>,&lt;key2>,... </code>
+</td>
+<td>
+Execute key command(s) (where each <code>&lt;key&gt;</code> is one of the keys
+listed under *player controls*, below).
+</td>
+</tr>
+<tr>
+<td>
+<code>--exit </code>
+</td>
+<td>
+Exit when playback begins or <code>--command &lt;key&gt;</code> completes.
+</td>
+</tr>
+<tr>
+<td>
+<code>--help </code>
+</td>
+<td>
+ Display help message.
+</td>
+</tr>
+</table>
 
-* `--tomp4` Transcode a video file to mp4 during playback. This option requires
-ffmpeg to be installed on your computer. The play / pause controls are currently
-not supported in transcode mode.
-
-* `--device "my chromecast"` If you have more than one Chromecast on your network,
-use the `--device` option to specify the device on which you want to start casting.
-Otherwise, castnow will just use the first device it finds in the network.
-
-* `--address 192.168.1.4` The IP address or hostname of your chromecast. This will skip
-the MDNS scan.
-
-* `--subtitles <path/URL>` This can be a path or URL to a vtt or srt file that
-contains subtitles.
-
-* `--subtitle-scale 1.5` Scaling factor for the size of the subtitle font. Default is 1.0.
-
-* `--subtitle-color #FFFFFFFF` Foreground RGBA color of the subtitle font.
-
-* `--myip 192.168.1.8` Your main IP address \(useful if you have multiple network adapters)
-
-* `--quiet` Hide the player timeline.
-
-* `--peerflix-<option> <argument>` Pass options to peerflix.
-
-* `--ffmpeg-<option> <argument>` Pass options to ffmpeg.
-
-* `--type <type>` Explicity set the mime-type of the first item in the playlist (e.g. 'video/mp4').
-
-* `--seek <hh:mm:ss>` Seek to the specified time on start using the format hh:mm:ss or mm:ss.
-
-* `--bypass-srt-encoding` Disable automatic UTF-8 encoding of SRT subtitles.
-
-* `--loop` Play the list of files over and over in a loop, forever.
-
-* `--shuffle` Play the list of files in random order.
-
-* `--recursive` List all files in directories recursively.
-
-* `--volume-step` Step at which the volume changes. Helpful for speakers that are softer or louder than normal. Value ranges from 0 to 1. Default is 0.05.
-
-* `--command <key1>,<key2>,...` Execute key command(s) (where each `<key>` is one of the keys listed under *player controls*, below).
-
-* `--exit` Exit when playback begins or `--command <key>` completes.
-
-* `--help` Display help message.
-
-Optionally, options can be preset by storing them in a file named `.castnowrc` in the current
-user's home directory.  For example:
+Optionally, options can be preset by storing them in a file named `.castnowrc`
+in the current user's home directory. For example:
 
 ```
 --myip=192.168.1.8
@@ -128,8 +312,9 @@ q       // quit
 
 ### YouTube Support
 
-We had to drop direct YouTube support for now since google changed the chromecast YouTube API.
-However, there is a nice workaround in combination with the tool [youtube-dl](https://github.com/rg3/youtube-dl):
+We had to drop direct YouTube support for now since google changed the
+Chromecast YouTube API. However, there is a nice workaround in combination with
+the tool [youtube-dl](https://github.com/rg3/youtube-dl):
 
 `youtube-dl -o - https://youtu.be/BaW_jenozKc | castnow --quiet -`
 
@@ -137,7 +322,8 @@ Thanks to [trulex](https://github.com/trulex) for pointing that out.
 
 ### Non-Interactive
 
-Castnow can also be used in cron jobs or via window-manager bindings; for example:
+Castnow can also be used in cron jobs or via window-manager bindings; for
+example:
 
 ```
 // Play/pause.
@@ -149,7 +335,8 @@ castnow --command up --exit
 
 #### Usage via [screen](https://www.gnu.org/software/screen/) command
 
-To avoid starting a new castnow command every time (which takes long time) you should use background sessions.
+To avoid starting a new castnow command every time (which takes long time) you
+should use background sessions.
 
 ```
 // run castnow in backgound only once:
@@ -191,9 +378,12 @@ screen -S cast_session -X quit
 
 ### reporting bugs/issues
 
-Please include the debug output in your issues. You can enable the debug messages by setting the
-DEBUG environment variable before running the castnow command like this: `DEBUG=castnow* castnow ./myvideo.mp4`.
-Some problems have already been addressed in our wiki https://github.com/xat/castnow/wiki.
+Please include the debug output in your issues. You can enable the debug
+messages by setting the DEBUG environment variable before running the castnow
+command like this: `DEBUG=castnow* castnow ./myvideo.mp4`. Some problems have
+already been addressed in our wiki https://github.com/xat/castnow/wiki.
+
+Please only report metadata-related issues here and general issues there.
 
 ### contributors
 
--- index.js
+++ index.js
@@ -7,12 +7,16 @@ var chalk = require('chalk');
 var keypress = require('keypress');
 var ui = require('playerui')();
 var circulate = require('array-loop');
-var xtend = require('xtend');
 var shuffle = require('array-shuffle');
 var unformatTime = require('./utils/unformat-time');
 var debug = require('debug')('castnow');
 var debouncedSeeker = require('debounced-seeker');
 var mime = require('mime');
+var btoa = require( "btoa" );
+var getUri = require( "get-uri" );
+var _ = require( "lodash" );
+var mm = require( "music-metadata" );
+var sharp = require( "sharp" );
 var noop = function() {};
 
 // plugins
@@ -31,8 +35,12 @@ try {
 } catch(err) {}
 
 var optConfig = {
-  boolean: "tomp4 quiet bypass-srt-encoding loop shuffle recursive exit help".split(/\s+/),
-  string: "device address subtitles subtitle-scale subtitle-color subtitle-port myip type seek volume-step localfile-port transcode-port torrent-port stdin-port command".split(/\s+/)
+  boolean: "tomp4 quiet bypass-srt-encoding loop shuffle recursive exit help showoptions metadata showmetadata showcover".split(/\s+/),
+  string: "device address subtitles subtitle-scale subtitle-color subtitle-port myip type seek volume-step localfile-port transcode-port torrent-port stdin-port command".split(/\s+/),
+  default: {
+    metadata: true,
+    showcover: true
+  }
 }
 var args = rcOpts.concat(process.argv.slice(2));
 var opts = require('minimist')(args, optConfig);
@@ -49,7 +57,7 @@ if (opts.help) {
     'Usage: castnow [<media>, <media>, ...] [OPTIONS]',
     '',
     'Option                   Meaning',
-    '--tomp4                  Convert file to mp4 during playback',
+    '--tomp4                  Convert media to mp4 during playback',
     '--device <name>          The name of the Chromecast device that should be used',
     '--address <ip>           The IP address or hostname of your Chromecast device',
     '--subtitles <path/url>   Path or URL to an SRT or VTT file',
@@ -65,15 +73,18 @@ if (opts.help) {
     '--seek <hh:mm:ss>        Seek to the specified time on start using the format hh:mm:ss or mm:ss',
     '--loop                   Loop over playlist, or file, forever',
     '--shuffle                Play in random order',
-    '--recursive              List all files in directories recursively',
+    '--recursive              List all media in directories recursively',
     '--volume-step <step>     Step at which the volume changes. Helpful for speakers that are softer or louder than normal. Value ranges from 0 to 1 (e.g. ".05")',
-    '--localfile-port <port>  Specify the port to be used for serving a local file',
-    '--transcode-port <port>  Specify the port to be used for serving a transcoded file',
-    '--torrent-port <port>    Specify the port to be used for serving a torrented file',
-    '--stdin-port <port>      Specify the port to be used for serving a file read from stdin',
+    '--localfile-port <port>  Specify the port to be used for serving local media',
+    '--transcode-port <port>  Specify the port to be used for serving transcoded media',
+    '--torrent-port <port>    Specify the port to be used for serving torrented media',
+    '--stdin-port <port>      Specify the port to be used for serving media read from stdin',
+    '--metadata false         Do not attempt to retrieve metadata from audio media',
+    '--showmetadata           Show metadata',
+    '--showcover false        Do not show cover art on console',
+    '--showoptions            Show values for options',
     '--command <key1>,<key2>  Execute key command(s) (where each <key> is one of the keys listed below)',
     '--exit                   Exit when playback begins or --command completes',
-
     '--help                   This help screen',
     '',
     'Player controls',
@@ -95,9 +106,10 @@ if (opts.help) {
 }
 
 if (opts._.length) {
-  opts.playlist = opts._.map(function(item) {
+  opts.playlist = opts._.map( function( media ) {
     return {
-      path: item
+      path: media,
+      contentUrl: media
     };
   });
 }
@@ -136,6 +148,145 @@ function fatalError(err) {
   process.exit();
 }
 
+function getPicture( media ) {
+  try {
+    return media.audioMetadata.common.picture[0].data
+  }
+  catch( err ) {
+    return undefined;
+  }
+}
+
+function pictureToConsole( image, bottom ) {
+  return new Promise( ( resolve, reject ) => {
+    if ( !image ){
+      return resolve( image );
+    }
+    sharp( image ).resize( {
+      fit: "inside",
+      height: process.stdout.rows * 2 - 8,
+      width: process.stdout.columns
+    } )
+    .raw()
+    .toBuffer( { resolveWithObject: true } )
+    .then( ( { data, info } ) => {
+      data = new Uint8Array( data );
+      const lineWidth = info.width * info.channels;
+      const lines = [];
+      const fgc = {};
+      const bgc = {};
+
+      for( let y = 0; y < info.height - 1; y += 2 ) {
+        const c = data.slice( lineWidth * y, lineWidth * ( y + 2 ) );
+        const line = c.slice( 0, lineWidth );
+        const nextLine = c.slice( lineWidth, lineWidth * 2 );
+
+        let pixels = [];
+        for( let idx = 0; idx < lineWidth; idx += info.channels ) {
+          const bgr = line[ idx ], bgg = line[ idx + 1 ], bgb = line[ idx + 2 ];
+          const bgKey = bgr << 16 | bgg << 8 | bgb;
+          let bg = bgc[ bgKey ];
+          if( !bg ) {
+            bg = chalk.bgRgb( bgr, bgg, bgb )( "_" ).split( "_" )[ 0 ];
+            bgc[ bgKey ] = bg;
+          }
+          const fgr = nextLine[ idx ], fgg = nextLine[ idx + 1 ], fgb = nextLine[ idx + 2 ];
+          const fgKey = fgr << 16 | fgg << 8 | fgb;
+          let fg = fgc[ fgKey ];
+          if( !fg ) {
+            fg = chalk.rgb( fgr, fgg, fgb )( "▄" );
+            fgc[ fgKey ] = fg;
+          }
+          pixels.push( bg + fg );
+        }
+        lines.push( pixels.join( "" ) );
+      }
+      console.log();
+      console.log( chalk.reset( lines.join( chalk.reset( "\n" ) ) ) );
+      if( bottom ) {
+        console.log( "\n".repeat( bottom - 1 ) );
+      }
+      return resolve();
+    } )
+  } )
+}
+
+// Limit image data to fit arbitrary protobuffer size of 64k.
+const maxSize = 32000;
+
+// Extract image and reduce until it fits the buffer size.
+async function processImage( data ) {
+  var size = data.length * 4 / 3;
+  return await sharp( data )
+  .metadata()
+  .then( metadata => {
+    const resize = {
+      execute: size > maxSize || metadata.height > 512 || metadata.width > 512,
+      i: 0,
+      fit: "inside",
+      height: Math.min( 512, metadata.height ),
+      width: Math.min( 512, metadata.width )
+    }
+
+    return resize;
+  } ).then( async resize => {
+    const result = {};
+    do {
+      const image = sharp( data );
+      if( resize.execute ) {
+        image.resize( resize );
+      }
+
+      await image
+      .jpeg()
+      .toBuffer()
+      .then( newImage => {
+        if( 0 === resize.i ) {
+          result.local = newImage
+        }
+        result.remote = "data:image/jpg;base64, " + btoa( newImage )
+      } );
+      resize.height -= 16;
+      resize.width -= 16;
+      resize.size = result.remote.length;
+    } while( resize.size > maxSize && resize.i++ < 32 )
+    return result;
+  } )
+}
+
+function getMetadata( media ) {
+  return new Promise( ( resolve, reject ) => {
+    // Only do this once per media item.
+    if ( !media || media.audioMetadata ) {
+      return resolve( media );
+    };
+    media.audioMetadata = {};
+
+    getUri( encodeURI( decodeURIComponent( media.contentUrl ) ), {}, ( err, stream ) => {
+      if ( err ) {
+        return reject( err );
+      };
+      mm.parseStream( stream ).then( async metadata => {
+        media.audioMetadata = metadata;
+        const mc = metadata.common;
+        media.metadata = {
+          metadataType: 3,
+          albumName: mc.album,
+          artist: mc.artist,
+          title: mc.title
+        }
+        if ( Array.isArray( mc.picture ) && mc.picture[0] ) {
+          await processImage( mc.picture[ 0 ].data ).then( result => {
+            mc.picture[ 0 ].data = result.local;
+            media.metadata.images = [ { url: result.remote } ];
+          } );
+        }
+        return resolve( media );
+      } )
+    } )
+  } )
+}
+
 var last = function(fn, l) {
   return function() {
     var args = Array.prototype.slice.call(arguments);
@@ -153,8 +304,8 @@ var ctrl = function(err, p, ctx) {
     process.exit();
   }
 
-  var playlist = ctx.options.playlist;
-  var playlist_history = ctx.options.playlist_history;
+  var playlist = ctx.options.playlist || [];
+  var playlist_history = ctx.options.playlist_history || [];
   var volume;
   var is_keyboard_interactive = process.stdin.isTTY || false;
 
@@ -206,14 +357,12 @@ var ctrl = function(err, p, ctx) {
           !status.media.metadata.title) return;
 
       var metadata = status.media.metadata;
-      var title;
-      if (metadata.artist) {
-        title = metadata.artist + ' - ' + metadata.title;
-      } else {
-        title = metadata.title;
-      }
-      ui.setLabel('source', 'Source', title);
-      ui.showLabels('state', 'source');
+      var title = metadata.title;
+      title = ( metadata.artist ? metadata.artist + " - " : "" ) + title;
+      title = title + ( metadata.album ? " (" + metadata.album + ")" : "" );
+
+      ui.setLabel( 'source', 'Source', title );
+      ui.showLabels( 'state', 'source' );
       ui.render();
     });
   };
@@ -235,15 +384,17 @@ var ctrl = function(err, p, ctx) {
   var nextInPlaylist = function() {
     if (ctx.mode !== 'launch') return;
     if (!playlist.length) return process.exit();
-    p.stop(function() {
+    p.stop( async function() {
       ui.showLabels('state');
-      debug('loading next in playlist: %o', playlist[0]);
-      p.load(playlist[0], noop);
-      var file = playlist.shift();
+      const media = playlist.shift();
+      debug( 'loading next in playlist: %o', media );
+      await pictureToConsole( getPicture( media ), 3 );
+      p.load( { media: _.pick( media, "contentId", "contentUrl", "contentType", "metadata" ) }, noop );
+
       if (ctx.options.loop)
-        playlist.push(file);
+        playlist.push( media );
       else
-        playlist_history.push(file);
+        playlist_history.push( media );
     });
   };
 
@@ -344,12 +495,12 @@ var ctrl = function(err, p, ctx) {
       });
     },
 
-    // next item in playlist
+    // next media in playlist
     n: function() {
       nextInPlaylist();
     },
 
-    // previous item in playlist
+    // previous media in playlist
     p: function() {
       previousInPlaylist();
     },
@@ -442,36 +593,81 @@ player.use(localfile);
 player.use(transcode);
 player.use(subtitles);
 
-player.use(function(ctx, next) {
-  if (ctx.mode !== 'launch') return next();
-  if (ctx.options.type)
-    // If a --type has been provided, then respect it.
-    ctx.options.playlist.map(function(item) {item.type = ctx.options.type;});
+player.use( function( ctx, next ) {
+  if( ctx.mode !== 'launch' ) return next();
+  if( ctx.options.type ) {
+    // If a --type has been provided, then force it.
+    ctx.options.playlist.map( function( media ) {
+       media.type = ctx.options.type;
+       media.contentType = ctx.options.type;
+     } );
+  }
   else {
-    ctx.options.playlist.map(function(item){
-      if (!item.type) {
-        // These will be URLs (the MIME type for files is filled in by the localfile plugin).
-        var mimeType = mime.lookup(item.path);
+    ctx.options.playlist.map( function( media ){
+      if( !media.type ) {
+        // These will be URLs (the MIME type for medias is filled in by the localfile plugin).
+        var mimeType = mime.lookup( media.contentUrl );
         var type = mimeType.split('/')[0];
-        if (type === 'audio' || type === 'video') item.type = mimeType;
+        if( type === 'audio' || type === 'video' ) {
+          media.type = mimeType;
+          media.contentType = mimeType;
+        }
       }
     });
   }
   next()
 });
 
-player.use(function(ctx, next) {
-  if (ctx.mode !== 'launch') return next();
-  if (ctx.options.shuffle)
-    ctx.options.playlist = shuffle(ctx.options.playlist);
-  ctx.options = xtend(ctx.options, ctx.options.playlist[0]);
-  var file = ctx.options.playlist.shift();
-  if (ctx.options.loop)
-    ctx.options.playlist.push(file);
+player.use( async function( ctx, next ) {
+  if( ctx.mode !== 'launch' ) return next();
+  if( ctx.options.shuffle ) {
+    ctx.options.playlist = shuffle( ctx.options.playlist );
+  }
+
+  var media = ctx.options.playlist.shift();
+  ctx.options.media = media;
+
+  if( ctx.options.loop )
+    ctx.options.playlist.push( media );
   else
-    ctx.options.playlist_history = [file];
+    ctx.options.playlist_history = [ media ];
+
+  if( opts.metadata && media.contentType.startsWith( "audio" ) ){
+    await getMetadata( media )
+    .then( () => {
+      if( media.audioMetadata ) {
+        ctx.options.media = _.pick( media, "contentId", "contentUrl", "contentType", "metadata" );
+        if( opts.showmetadata ) {
+          console.log( media.audioMetadata );
+        }
+        if( opts.showcover ) {
+          pictureToConsole( getPicture( media ) );
+        }
+      }
+      next();
+    } )
+    .catch( ( err ) => {
+        console.log( chalk.redBright( "Error: " + err.message + ". " ) + media.contentUrl );
+        next();
+    } );
+  }
   next();
 });
+
+player.use( function( ctx, next ) {
+  if( ctx.mode === 'launch' && opts.metadata ) {
+    ctx.options.playlist.forEach( media => {
+      if( !media.audioMetadata && media.contentType.startsWith( "audio" ) ) {
+        getMetadata( media );
+      }
+    } );
+  }
+  next();
+} );
+
+if( opts.showoptions ) {
+  console.log( opts );
+}
 
 if (!opts.playlist) {
   debug('attaching...');
